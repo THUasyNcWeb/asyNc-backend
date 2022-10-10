868684d84734837cf71017a5e314fed8045f389e
宋曦轩 <2021010728@secoder.net>
2022-10-11 00:00:44 +0800

    :sparkles: feat: finished and tested the very basic framework

--- asyNc/main/views.py
+++ asyNc/main/views.py
@@ -17,7 +17,7 @@ def index(request):
         pass
     else:
         print("Any thing new?")
-    return JsonResponse({"code": 200, "data": "Hello World"})
+    return JsonResponse({"code": 200, "data": "Hello World"}, status = 200)
 
 #user login
 @csrf_exempt
@@ -46,16 +46,25 @@ def user_login(request):
             return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
         user_name = request_data["user_name"]
         password = request_data["password"]
-        response_msg = {
-            "code": 0,
-            "message": "SUCCESS",
-            "data": {
-                "id": 1,
-                "user_name": user_name,
-                "token": "SECRET_TOKEN"
+        if user_name == "Alice" and password == "Bob19937": # should use md5
+            status_code = 200
+            response_msg = {
+                "code": 0,
+                "message": "SUCCESS",
+                "data": {
+                    "id": 1,
+                    "user_name": user_name,
+                    "token": user_name # transient measures, need to be changed.
+                }
             }
-        }
-        return JsonResponse(response_msg, status = 200)
+        else:
+            status_code = 400
+            response_msg = {
+                "code": 4,
+                "message": "WRONG_PASSWORD",
+                "data": {}
+            }
+        return JsonResponse(response_msg, status = status_code)
     
     return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
 
@@ -86,17 +95,39 @@ def user_register(request):
             return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
         user_name = request_data["user_name"]
         password = request_data["password"]
-        response_msg = {
-            "code": 0,
-            "message": "SUCCESS",
-            "data": {
-                "id": 1,
-                "user_name": user_name,
-                "token": "SECRET_TOKEN"
+        if user_name == "Alice":
+            status_code = 400
+            response_msg = {
+                "code": 1,
+                "message": "USER_NAME_CONFLICT",
+                "data": {}
             }
-        }
-        return JsonResponse(response_msg, status = 200)
-    
+        elif not type(user_name) == str:
+            status_code = 400
+            response_msg = {
+                "code": 2,
+                "message": "INVALID_USER_NAME_FORMAT",
+                "data": {}
+            }
+        elif not type(password) == str:
+            status_code = 400
+            response_msg = {
+                "code": 3,
+                "message": "INVALID_PASSWORD_FORMAT",
+                "data": {}
+            }
+        else:
+            status_code = 200
+            response_msg = {
+                "code": 0,
+                "message": "SUCCESS",
+                "data": {
+                    "id": 1,
+                    "user_name": user_name,
+                    "token": user_name # transient measures, need to be changed.
+                }
+            }
+        return JsonResponse(response_msg, status = status_code)
     return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
 
 # return a news list
@@ -104,36 +135,33 @@ def user_register(request):
 def news_response(request):
     """
     response:
-    [
-        {
-            "title":String,
-            // 新闻的标题
-            "url":String,
-            // 新闻的网址
-            "category":String,
-            // 新闻所属的类别，如政治、生活
-            "priority":int,
-            // 新闻的优先级，在前端以字号大小加以区分
-    		// 现在认为优先级分三类，1代表最高级，2代表次高级，3代表一般级
-            // 初步认为仅在“热点”这一类别中有最高级的新闻，整个正文中有且仅有一条最高级
-            // "热点"中次高级数目不限，其他类别次高级新闻为2条
-            // 所有类别的一般级新闻不限
-        }
-    ]
+    {
+        "code": 0,
+        "message": "SUCCESS",
+        "data": [
+            {
+                "title": "Breaking News",
+                "url": "https://breaking.news",
+                "category": "breaking",
+                "priority": 1,
+                "picture_url": "https://breaking.news/picture.png"
+            }
+        ]
+    }
     """
     if request.method == "GET":
         token = request.META.get("HTTP_AUTHORIZATION")
         # print("token :", token) # delete this before deploy
         newses = []
         news = {
-            "title":"A good titile is all you need.",
-            "url":"a.good.titile.is.all.you.need",
-            "category":"tutorial",
-            "priority":1,
+            "title": "Breaking News",
+            "url": "https://breaking.news",
+            "category": "breaking",
+            "priority": 1,
+            "picture_url": "https://breaking.news/picture.png"
         }
         newses.append(news)
-        newses.append(news)
-        return JsonResponse({"code": 200, "data": newses})
+        return JsonResponse({"code": 0, "message": "SUCCESS", "data": newses}, status = 200)
     elif request.method == "POST":
         print("Not Right.")
     else:
@@ -148,17 +176,42 @@ def user_modify_password(request):
     """
     request:
     {
-    "user_name": "Alice",
-    "old_password": "Bob19937",
-    "new_password": "Carol48271"
+        "user_name": "Alice",
+        "old_password": "Bob19937",
+        "new_password": "Carol48271"
     }
     """
-    try:
-        token = request.META.get("HTTP_AUTHORIZATION")
-    except Exception as e:
-        return JsonResponse({"code": 1000, "message": "NOT_FOUND", "data": {}}, status = 401)
-    try:
-        request_data = json.loads(request.body.decode())
-    except Exception as e:
-        return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
-    return JsonResponse({"code": 0, "message": "SUCCESS", "data": {}}, status=200)
+
+    if request.method == "POST":
+        try:
+            token = request.META.get("HTTP_AUTHORIZATION")
+        except Exception as e:
+            return JsonResponse({"code": 1000, "message": "NOT_FOUND", "data": {}}, status = 401)
+        try:
+            request_data = json.loads(request.body.decode())
+        except Exception as e:
+            return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
+        
+        user_name = request_data["user_name"]
+        old_password = request_data["old_password"]
+        new_password = request_data["new_password"]
+            
+        if not old_password == "Bob19937": # should use md5
+            status_code = 400
+            response_msg = {
+                "code": 4,
+                "message": "WRONG_PASSWORD",
+                "data": {}
+            }
+        elif not type(new_password) == str: # should use md5
+            status_code = 400
+            response_msg = {
+                "code": 3,
+                "message": "INVALID_PASSWORD_FORMAT",
+                "data": {}
+            }
+        else:
+            status_code = 200
+            response_msg = {"code": 0, "message": "SUCCESS", "data": {}}
+        return JsonResponse(response_msg, status = status_code)
+    return JsonResponse({"code": 1003, "message": "INTERNAL_ERROR", "data": {}}, status = 500)
