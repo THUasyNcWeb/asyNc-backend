d282bacc5a7d58a5b9302d2bd76830d0f0b6e4ec
宋曦轩 <2021010728@secoder.net>
2022-10-18 22:25:55 +0800

    :test_tube: test: add a test to check md5 function in tools asyNc/asyNc-web#44

--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,23 @@
     test.py in django frame work
 """
 
-# from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
+from .tools import *
 
 # Create your tests here.
+
+
+class UserModelTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_tools_md5(self):
+        title = "Test"
+        content = "test tools.md5 function"
+
+        str_list = ["password", "Bob19937", "Carol48271", "123456"]
+        for string in str_list:
+            md5_calculator = hashlib.md5()
+            md5_calculator.update(string.encode(encoding='UTF-8'))
+            self.assertEqual(md5(string), md5_calculator.hexdigest())
