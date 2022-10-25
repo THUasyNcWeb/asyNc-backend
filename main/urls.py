6ba135fe9da5809f69652024edbd636a3e51003d
尚子智 <2020010852@secoder.net>
2022-10-25 14:38:22 +0800

    :sparkles: feat: add url for search asyNc-web#73

--- main/urls.py
+++ main/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('login/', views.user_login),
     path('register/', views.user_register),
     path('modify_password/', views.user_modify_password),
+    path('search/', views.keyword_search),
 ]
