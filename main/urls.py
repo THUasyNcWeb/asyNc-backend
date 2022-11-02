f44807ae96831c77a4a9b1a5b61a387bd2d88dc2
宋曦轩 <2021010728@secoder.net>
2022-11-02 21:45:57 +0800

    :recycle: style: fix url style asyNc-web#146

--- main/urls.py
+++ main/urls.py
@@ -7,14 +7,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
-    path('all_news/', views.news_response),
-    path('login/', views.user_login),
-    path('register/', views.user_register),
-    path('modify_password/', views.user_modify_password),
-    path('checklogin/', views.check_login_state),
+    path('index', views.index),
+    path('all_news', views.news_response),
+    path('login', views.user_login),
+    path('register', views.user_register),
+    path('modify_password', views.user_modify_password),
+    path('checklogin', views.check_login_state),
     path('search', views.keyword_search),
-    path('logout/', views.user_logout),
-    path('modify_username/', views.user_modify_username),
-    path('user_info/', views.user_info)
+    path('logout', views.user_logout),
+    path('modify_username', views.user_modify_username),
+    path('user_info', views.user_info)
 ]
