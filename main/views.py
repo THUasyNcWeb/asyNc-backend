7d8ea1e2ed2b09084595dd8371dd6e378b51131a
宋曦轩 <2021010728@secoder.net>
2022-11-02 18:53:40 +0800

    :sparkles: feat: update get method for user_info api asyNc-web#142

--- main/views.py
+++ main/views.py
@@ -227,22 +227,23 @@ def user_info(request):
                     "C++",
                     "中年",
                     "アニメ"
-                ]
+                ],
+                "mail": "waifu@diffusion.com",
+                "avatar": "",
             }
         ]
     }
     """
     try:
+        encoded_token = str(request.META.get("HTTP_AUTHORIZATION"))
+        token = tools.decode_token(encoded_token)
+        if not tools.check_token_in_white_list(encoded_token=encoded_token):
+            return unauthorized_response()
+    except Exception as error:
+        print(error)
+        return unauthorized_response()
+    try:
         if request.method == "GET":
-            try:
-                encoded_token = str(request.META.get("HTTP_AUTHORIZATION"))
-                token = tools.decode_token(encoded_token)
-                if not tools.check_token_in_white_list(encoded_token=encoded_token):
-                    return unauthorized_response()
-            except Exception as error:
-                print(error)
-                return unauthorized_response()
-
             try:
                 user_name = token["user_name"]
                 user = UserBasicInfo.objects.filter(user_name=user_name).first()
@@ -265,8 +266,10 @@ def user_info(request):
                     "data": {
                         "id": user.id,
                         "user_name": user.user_name,
-                        "signature": "This is my signature.",
-                        "tags": user_tags[:10]
+                        "signature": user.signature,
+                        "tags": user_tags[:10],
+                        "mail": user.mail,
+                        "avatar": user.avatar,
                     }
                 }
                 return JsonResponse(
